918c30c303f69004b7358e011de531f93aec3b56
Matheus Woeffel <matheuswoeffel@gmail.com>
2021-02-02 21:00:32 -0300

    Added diff reporting

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -17,11 +17,17 @@ cp etapa1 tests
 cp etapa1Gold tests
 
 cd tests
-mkdir saidas
+mkdir -p saidas
 for file in $filesToTest 
 do
     cat $file | ./etapa1 > saidas/${file}.txt
     cat $file | ./etapa1Gold > saidas/${file}.txt.Gold
+done
+
+for file in $(ls saidas | grep ".txt"$)
+do
+    echo "-----------------${file}-----------------"
+    diff saidas/${file}  saidas/${file}.Gold
 done
 
 
