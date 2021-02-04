3ef157bc778000857b83ecdbe9a2088fee7c4df9
Matheus Woeffel <matheuswoeffel@gmail.com>
2021-02-03 21:03:58 -0300

    New tests

--- tests/multiline_test.c
+++ tests/multiline_test.c
@@ -7,4 +7,18 @@
 	/* two comments on one line */ /* NOT one long comment */
 	/* a comment with a silly character at the end **/
     /******** ajdjdjfkajd jjjjjjj*****/
+	/**/ Incorrectly removed /**/
+	int/* comment */x;
+	 "/* this is not a comment. */"
+	 /***** comment ******/
+	 /*
+ * Another common multi-line comment style.
+ */
+
+/****
+ * Common multi-line comment style.
+ ****/
+// /*
+some_code();
+// */
     .
