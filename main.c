d6d72b3c0a63ef82e60f724911e330fccb02c354
Alencar da Costa <alencaradc@gmail.com>
2021-03-05 10:32:49 -0300

    Added new main file

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 /*
-Função principal para realização da análise sintática.
-
-Este arquivo será posterioremente substituído, não acrescente nada.
+  Função principal para realização da E3.
+  Não modifique este arquivo.
 */
 #include <stdio.h>
-#include "parser.tab.h" //arquivo gerado com bison -d parser.y
+extern int yyparse(void);
 extern int yylex_destroy(void);
+
+void *arvore = NULL;
+void exporta (void *arvore);
+void libera (void *arvore);
 
 int main (int argc, char **argv)
 {
-  int ret = yyparse();
+  int ret = yyparse(); 
+  exporta (arvore);
+  libera(arvore);
+  arvore = NULL;
   yylex_destroy();
   return ret;
 }
