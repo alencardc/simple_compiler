45cb08872450d1869d9e1b0ec591faf0fab3abe2
Alencar da Costa <alencaradc@gmail.com>
2021-03-05 17:16:50 -0300

    Implemented tree data structure

--- utils/tree.c
+++ utils/tree.c
@@ -1,3 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "tree.h"
+
+Node* create_node(Value *data) {
+  Node *node;
+  node = (Node*) malloc(sizeof(Node));
+  if (node == NULL) {
+    return NULL;
+  }
+
+  node->data = data;
+  node->parent = NULL;
+  node->next = NULL;
+  node->prev = NULL;
+  node->children = NULL;
+
+  return node;
+}
+
+Node* append_child(Node *parent, Node *child) {
+  if (parent == NULL || child == NULL) {
+    return NULL;
+  }
+  
+  child->parent = parent;
+
+  if (parent->children == NULL) {
+    parent->children = child;
+    return child;
+  }
+
+  Node* sibling = parent->children;
+  while (sibling->next != NULL) {
+    sibling = sibling->next;
+  }
+  sibling->next = child;
+  child->prev = sibling;
+
+  return child;
+}
+
+bool is_root(Node *node) {
+  return node != NULL && node->parent == NULL;
+}
+
+void free_node(Node *node) {
+  if (node == NULL) {
+    return;
+  }
+
+  if (is_root(node) == false) {
+    // Unlink node from siblings and parent
+    if (node->prev == NULL) {
+      node->parent->children = node->next;
+    } else {
+      node->prev->next = node->next;
+
+      if (node->next != NULL) {
+        node->next->prev = node->prev;
+      }
+    }
+  }
+
+  node->next = NULL;
+  node->prev = NULL;
+  free_node_rec(node);
+}
+
+static void free_node_rec(Node *node) {
+  if (node == NULL) {
+    return;
+  }
+
+  if (node->children != NULL) {
+    free_node_rec(node->children);
+  } 
+
+  if (node->next != NULL) {
+    free_node_rec(node->next);
+  } 
+
+  free(node);
+  node = NULL;
+}
+
+
 void exporta(void *root) {
   //todo
 }
--- utils/tree.h
+++ utils/tree.h
@@ -1,3 +1,31 @@
+#ifndef __TREE_H__
+#define __TREE_H__
+
+#include <stdbool.h>
+
+typedef struct sValue {
+  int i_value;
+  float f_value;
+} Value;
+
+typedef struct sNode {
+  Value *data;
+  struct sNode *parent;
+  struct sNode *next;
+  struct sNode *prev;
+  struct sNode *children;
+} Node;
+
+Node* create_node(Value *data);
+Node* append_child(Node *parent, Node *child);
+
+bool is_root(Node *node);
+
+void free_node(Node *node);
+static void free_node_rec(Node *node);
+
 
 void exporta(void *root);
 void libera(void *root);
+
+#endif // __TREE_H__
